8691d5a1499a4038b66c3e47d9ac7a776101a8ec
TurtlePower <agnkvdm@gmail.com>
2023-06-26 00:05:58 +0300

    add pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import Home from 'pages/Home';
+import MovieDetails from 'pages/MovieDetails';
+import Movies from 'pages/Movies';
+import { NavLink, Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies">Movies</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route indexed element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />} />
+        <Route path="*" element={<div>Not found</div>} />
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/MovieDetails.jsx
@@ -0,0 +1,5 @@
+const MovieDetails = () => {
+  return <div>MovieDetails page</div>;
+};
+
+export default MovieDetails;
